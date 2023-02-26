05de53e0956a1c056021e876c6d4f1ccff395f3e
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-02-26 18:40:43 +0000

    commented

--- Programming/assigjava/FullTimeStudent.java
+++ Programming/assigjava/FullTimeStudent.java
@@ -1,18 +1,28 @@
-package sac;
+package sac;//package location 
 
-class FulltimeStudent extends Student {
+class FulltimeStudent extends Student {//start class 
+
+    //declare a private String variable 
     private String fullTime;
 
+    //start a constructor 
     public FulltimeStudent(String name, int id, String fullTime) {
-        super(name, id);
+        super(name, id); // super that takes two parameters 
+        //this variable equals this parameter 
         this.fullTime = fullTime;
-    }
 
-    public String isFullTime() {
-        return fullTime;
-    }
+    }//end constructor 
 
-    public void setFullTime(String fullTime) {
-        this.fullTime = fullTime;
-    }
-}
+    public String isFullTime() {//start return method 
+
+        return fullTime;//value returned 
+
+    }//end method 
+
+    public void setFullTime(String fullTime) {//start method 
+
+        this.fullTime = fullTime;//the variable equals the parameter 
+
+    }//end the method 
+
+}//end the class 
