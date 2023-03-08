b32a49208477f60fb89caeee50ed137752df0bc9
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2023-03-08 21:18:19 +0000

    .

--- Programming/assigjava/FullTimeStudent.java
+++ Programming/assigjava/FullTimeStudent.java
@@ -1,4 +1,4 @@
-package sac;//package location 
+package sad;
 
 class FulltimeStudent extends Student {//start class 
 
@@ -24,5 +24,27 @@ class FulltimeStudent extends Student {//start class
         this.fullTime = fullTime;//the variable equals the parameter 
 
     }//end the method 
+    
+    static boolean delayedText = true;
+
+    public static void Slow() throws InterruptedException {
+
+        delayedOutput("""
+            Welcome to College App \nDeveloped by Eskandar Atrakchi\nPlease select one of the following options below:
+            """);
+
+    }
+
+    public static void delayedOutput(String mes) throws InterruptedException {
+        if (delayedText) {
+            for (int i = 0; i < mes.length(); i++) {
+                System.out.print(mes.charAt(i));
+                Thread.sleep(50);
+            }
+            System.out.println();
+        } else {
+            System.out.println(mes);
+        }
+    }
 
 }//end the class 
